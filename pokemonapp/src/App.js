9bdff9fdf9b58277d58539c22a5bf1da659c8979
AybukeGrr <ce.aybukegurer@gmail.com>
2023-12-04 22:15:29 +0300

    Project installation has been completed. Project developments were made.

---
+++ pokemonapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './PokeCard.css';
+import PokeCard from './PokeCard';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokedex/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemonapp/src/PokeCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./PokeCard.css";
+
+const POKE_API_URL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+class PokeCard extends Component {
+  render() {
+    let imgSrc = `${POKE_API_URL}${this.props.id}.png`;
+    return (
+      <div className="PokeCard">
+        <h1 className="PokeCard-title">{this.props.name}</h1>
+        <div className="PokeCard-img">
+          <img src={imgSrc} alt={this.props.name}/>
+        </div>
+        <div className="PokeCard-data"> Type: {this.props.type}</div>
+        <div className="PokeCard-data"> Exp: {this.props.exp} </div>
+      </div>
+    );
+  }
+}
+
+export default PokeCard;
